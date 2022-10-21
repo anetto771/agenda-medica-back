da264e7ccdf4e20d1c6dcf0bdf3dbc2164042d75
Netto <anetto771@gmail.com>
2022-10-21 14:02:31 -0300

    Commit Innitial

---
+++ src/main/java/com/api/agendamedicaback/AgendaMedicaBackApplication.java
@@ -0,0 +1,13 @@
+package com.api.agendamedicaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaMedicaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaMedicaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/agendamedicaback/AgendaMedicaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.agendamedicaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaMedicaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
