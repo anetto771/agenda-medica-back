ef5e4592b5493974e82f7a666515a2cccd55f403
Netto <anetto771@gmail.com>
2022-10-24 16:32:38 -0300

    Criação da Classe Usuario

--- src/main/java/com/api/agendamedicaback/domain/Medico.java
+++ src/main/java/com/api/agendamedicaback/domain/Medico.java
@@ -1,12 +1,19 @@
 package com.api.agendamedicaback.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Medico extends Pessoa{
+
+    private static final long serialVersionUID = 1L;
+    @OneToMany(mappedBy = "medico")
     private List<Agendamento> agendamentos = new ArrayList<>();
 
     public Medico() {
+
         super();
     }
 
@@ -15,6 +22,7 @@ public class Medico extends Pessoa{
     }
 
     public List<Agendamento> getAgendamentos() {
+
         return agendamentos;
     }
 
